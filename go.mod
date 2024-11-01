90b3d7b759cb5ac6145c1065ed6876ee795c5ae7
Ben <code.by.ben@gmail.com>
2024-11-02 07:54:35 +1100

    functional ls

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bengarrett/namezed
 
 go 1.23.2
 
-require github.com/alecthomas/kong v1.2.1 // indirect
+require (
+	github.com/alecthomas/kong v1.2.1
+	github.com/charlievieth/fastwalk v1.0.9
+)
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/alecthomas/kong"
+	"github.com/charlievieth/fastwalk"
 )
 
 var cli struct {
@@ -18,9 +23,12 @@ var cli struct {
 	} `cmd:"" help:"Remove files."`
 
 	Ls struct {
+		Name  string   `arg:"" optional:"" help:"Filename to match."`
 		Paths []string `arg:"" optional:"" help:"Paths to list." type:"path"`
 	} `cmd:"" help:"List paths."`
 }
+
+// ls, cp, lsz, cpz
 
 func main() {
 	ctx := kong.Parse(&cli,
@@ -35,6 +43,37 @@ func main() {
 	case "rm <path>":
 		fmt.Println(cli.Rm.Paths, cli.Rm.Force, cli.Rm.Recursive)
 
-	case "ls":
+	case "ls <name> <paths>":
+		fmt.Println(">", cli.Ls.Name, cli.Ls.Paths)
+
+		// Follow links if the "-L" flag is provided
+		conf := fastwalk.Config{
+			Follow: true,
+		}
+
+		walkFn := func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %v\n", path, err)
+				return nil // returning the error stops iteration
+			}
+			if cli.Ls.Name != "" {
+				pattern := strings.ToLower(cli.Ls.Name)
+				name := strings.ToLower(d.Name()) // basename
+				if ok, err := filepath.Match(pattern, name); !ok {
+					// invalid pattern (err != nil) or name does not match
+					return err
+				}
+			}
+			_, err = fmt.Println(path)
+			return err
+		}
+		root := cli.Ls.Paths[0]
+		if err := fastwalk.Walk(&conf, root, walkFn); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", root, err)
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Println("unknown command:", ctx.Command())
 	}
 }
